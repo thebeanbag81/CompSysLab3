48df71e1d93c3d0b22432ba08ef91a96a3eb5853
thebeanbag81 <ehart@umass.edu>
2018-11-20 22:13:10 -0500

    Add files via upload

--- lab3.c
+++ lab3.c
@@ -224,12 +224,72 @@ main (int argc, char *argv[]){
     //has to be flexible with empty spaces
     //writes what was read into the array representing IM 
     //turns add    $s0, $s1, $s2 into add $s0 $s1 $s2 
+    //lw $s0, 8($t0) --> lw $s0 8 $t0 
 	/*********************************************/
 	char* progScanner(char *c){
 
         char[] delimiters = [',',' ','\n','\r',',,']; //delimiter array
+        char **token;
+        token=(char**)malloc(4*sizeof(char*));        
+        char* formStr = "";
+        
+        //finds opcode
+        int i = 0;
+        token[i] = strtok(c,delimiters);
+        token[0]=(char*)malloc(2*sizeof(char));
+        i++;
 
+        //checks too see if there is an opcode
+        if(token[0] == "") return formStr;
+        
+        //finds rest of instruction
+        while(token[i]!=NULL){
+            token[i]=strtok(NULL,delimiters);
+            token[i]=(char*)malloc(256*sizeof(char));
+            i++;
+        }
+        //if there are more than 4 parts of the instruction it is invalid
+        if(i>4)return formStr;
 
+        //checks to see if opcode is lw or sw
+        //then checks to see if parentheses are correct
+        //if they are, removes them from character array and returns 
+        //if not returns ERROR
+        if((my_strcmp(token[0],"lw"))==1 || (my_strcmp(token[0],"sw"))==1){
+           char* paren = token[2];
+           int j = 0;
+           int leftBracket = 0;
+           int rightBracket = 0;
+           char* foundL;
+           char* foundR;
+           while(paren[j]!=NULL){
+           foundL=strchr(paren[j],'(');
+           foundR=strchr(paren[j],')');
+            if(foundL) leftBracket++;
+            if(foundR) rightBracket++;
+           }
+           if(leftBracket!=rightBracket) {return formStr;} 
+           else{
+               int k = 0;
+               while(paren[k]!=NULL){
+                   if((paren[k]==')') || (paren[k]=='(')) paren[k]=" ";
+               }
+           }
+           
+
+        }
+
+        //pull string together
+        int l = 0;
+        while(l<i*2){
+        formStr[l] = &token[l];
+        formStr[++l]=" ";
+        l++;
+        }
+
+        free(token);
+        return formStr;
+        
 
 	}
 
