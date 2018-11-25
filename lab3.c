d1e480b96cab30b61ad0f386a61f403cf1526d7a
thebeanbag81 <ehart@umass.edu>
2018-11-24 19:23:46 -0500

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -454,17 +454,31 @@ main (int argc, char *argv[]){
         //reg number out of bounds 
         //immediate field too large
     /******************************************************************/
-	struct instruction parser(char *input){
-        char array[] = "add 16 17 18"; 
-char delimiters[] = ' ';
+struct instruction parser(char *input){
+    
+         //char array[] = "addi $s0 $s1 8"; 
+        char formArray[] = progScanner(array);
 
-char formArray[] = progScanner(array);
+        char delimiters[] = {' '}; //delimiter array
+        char **token;
+        token=(char**)malloc(10*sizeof(char*));        
+        
+        int i = 0;
+	    token[0]=(char*)malloc(256*sizeof(char));
+        token[i] = strtok(formStr,delimiters);
+    
+        while(token[i]!=NULL){
+        token[++i]=(char*)malloc(256*sizeof(char));   
+	    token[i]=strtok(NULL,delimiters);
+            
+        }
+
 
 char* op = token[0];
-char rs = token[1];
-char rt = token[2];
+char* rs = token[1];
+char* rt = token[2];
 
-struct inst instr;
+struct instr inst;
 
 switch(op[0]){
 
@@ -499,7 +513,7 @@ case 'm':
 if(op[1]=='u'){
   	if(op[2]=='l'){
 	   if(op[3]=='t'){
-		inst.opcode=mult
+		inst.opcode=mult;
 			}
 		}
 	}
@@ -510,7 +524,7 @@ case 'b':
 {
 if(op[1]=='e'){
   	if(op[2]=='q'){
-	   inst.opcode=mult
+	   inst.opcode=mult;
 		}
 	}
 else return NULL;
@@ -519,7 +533,7 @@ break;
 case 'l':
 {
 if(op[1]=='w'){
-  inst.opcode=mult
+  inst.opcode=mult;
 	}
 else return NULL;
 }
@@ -529,30 +543,31 @@ break;
 
 if(inst.opcode == lw || inst.opcode == sw){
 instr.rt = regNumberConverter(token[1]);
-instr.immediate = atoi(token[2]);
+inst.immediate = atoi(token[2]);
 instr.rs = regNumberConverter(token[3]);
-
 }
+
 else if(inst.opcode == beq){
 instr.rs = regNumberConverter(token[1]);
 instr.rt = regNumberConverter(token[2]);
-instr.immediate = atoi(token[3]);
+inst.immediate = atoi(token[3]);
 }
+
 else if(inst.opcode == addi){
 instr.rt = regNumberConverter(token[1]);
 instr.rs = regNumberConverter(token[2]);
-instr.immediate = atoi(token[3]);
+inst.immediate = atoi(token[3]);
 }
+
 else{
 instr.rd = regNumberConverter(token[1]);
 instr.rs = regNumberConverter(token[2]);
 instr.rt = regNumberConverter(token[3]); 
-
 }
 
-return instr;
+return inst;
 
-	}
+}
 
 	int IF(){
 
